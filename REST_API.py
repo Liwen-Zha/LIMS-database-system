9a712117bc6de2ce7132defb5fc2c000fb0dc3a2
Liwen <lz1820@ic.ac.uk>
2021-07-27 01:58:20 +0100

    Correct all the HTTP methods in the REST_API.py, especially the POST method; Add two command lines into test_server.sh to test the functions in the API design.

--- REST_API.py
+++ REST_API.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, abort
+import backend
 
 app = Flask(__name__)
 
@@ -6,21 +7,64 @@ app = Flask(__name__)
 def home():
     return '<h1>Start Page</h1>'
 
-@app.route('/ttt', methods=['POST'])
-def create_task():
-    if not request.json:
-        print('Failed')
-        #abort(400)
-    '''task = {
-        'sample_ID': request.json['sample_ID'],
-        'description': request.json.get('description', ""),
-        'done': True
-    }'''
-    return jsonify({'task':True}), 201
+# Register sample
+@app.route('/sample-registration', methods=['POST'])
+def register_sample():
+    dict_value = request.get_json()
+    json_value = jsonify(dict_value)
+    new_sample = backend.insert(dict_value['sample_type'], dict_value['sample_ID'], dict_value['loc'],
+                                dict_value['status'], dict_value['Q'], dict_value['unit'],
+                                dict_value['custodian'])
+    if new_sample:
+        return 'Successfully added {} to the database.'.format(dict_value['sample_ID'])
+    else:
+        return jsonify({"errorMsg": "Failed add sample"}), 400
 
-@app.route('/ttt', methods=['GET'])
-def get_task():
-    return '<h1>ttt page</h1>'
+# Search sample
+@app.route('/sample-search', methods = ["POST"])
+def search_sample():
+    dict_value = request.get_json()
+    searched_sample = backend.search_improved(dict_value['sample_type'], dict_value['sample_ID'],
+                                              dict_value['loc'],dict_value['status'],
+                                              dict_value['Q'], dict_value['unit'],
+                                              dict_value['custodian'])
+    if searched_sample:
+        print(searched_sample)
+        return 'Successfully searched {} in the database.'.format(dict_value['sample_ID'])
+    else:
+        return jsonify({"errorMsg": "Failed search sample"}), 400
+
+# View all sample logs
+@app.route('/all-logs', methods = ["GET"])
+def view_logs():
+    all_logs = backend.view_logs()
+
+    if all_logs:
+        return 'Successfully view the logbook of the database.'
+    else:
+        return jsonify({"errorMsg": "Failed view the logbook"}), 400
+
+# View all samples
+@app.route('/all-samples', methods = ["GET"])
+def view_samples():
+    all_samples = backend.view_samples()
+
+    if all_samples:
+        return 'Successfully view all samples in the database.'
+    else:
+        return jsonify({"errorMsg": "Failed view all samples"}), 400
+
+# Check the latest info of certain sample/freezer/custodian
+@app.route('/check', methods = ["POST"])
+def check_db():
+    dict_value = request.get_json()
+    checked_data = backend.check(dict_value['sample_type'], dict_value['sample_ID'], dict_value['loc'],
+                                 dict_value['status'], dict_value['custodian'])
+    if checked_data:
+        return 'Successfully checked status.'
+    else:
+        return jsonify({"errorMsg": "Failed check status"}), 400
+
 
 if __name__ == '__main__':
     app.run()
--- test_server.sh
+++ test_server.sh
@@ -12,3 +12,20 @@ curl -v --header "Content-Type: application/json" --request GET http://127.0.0.1
 
 # Testing PUT 
 curl -v --header "Content-Type: application/json" --request PUT http://127.0.0.1:5000/route4/bob
+
+# Testing route: sample-registration in backup-api
+curl -v --header "Content-Type: application/json" --request POST --data '{"sample_type":"blood","sample_ID":"blo123","loc":"f1","status":"ok","Q":4,"unit":"ml","custodian":"charles"}' http://127.0.0.1:5000/sample-registration
+
+# Testing route: sample-search in backup-api
+curl -v --header "Content-Type: application/json" --request POST --data '{"sample_type":"blood","sample_ID":"blo123","loc":"","status":"","Q":"","unit":"","custodian":""}' http://127.0.0.1:5000/sample-search
+
+# Testing route: all-logs in backup-api
+curl -v --header "Content-Type: application/json" --request GET http://127.0.0.1:5000/all-logs
+
+# Testing route: all-samples in backup-api
+curl -v --header "Content-Type: application/json" --request GET http://127.0.0.1:5000/all-samples
+
+# Testing route: check in backup-api
+curl -v --header "Content-Type: application/json" --request POST --data '{"sample_type":"","sample_ID":"blo123","loc":"","status":"","Q":"","unit":"","custodian":""}' http://127.0.0.1:5000/check
+
+
