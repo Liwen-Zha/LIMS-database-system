159310a094d4204732f7d37b601acda3a9c045b3
Liwen <lz1820@ic.ac.uk>
2021-08-06 07:03:23 +0100

    App.js: the home page (not complete yet) App_init.js: the initial file created by react App_simpleDemo.js: a simple design of a web page that allows me to understand how to operate react basically
    Please only focus on App.js (i.e., regardless of App_init.js and App_simpleDemo.js)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,76 @@
 import './App.css';
 import React from "react";
+import Log from "./pages/Log.jsx";
+import {
+    Route,
+    HashRouter
+} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
 function App() {
   //console.log("hello")
     return (
-        <body className="d-flex w-100 h-100 text-center text-white bg-dark">
+        <HashRouter>
+        <body class="d-flex text-center text-white bg-dark">
 
-        <div className="main-page-config d-flex w-100 h-100 p-3 mx-auto flex-column">
-            <header className="mb-auto">
+        <div class="home-page-config d-flex p-3 mx-auto flex-column">
+            <header class="mb-auto">
                 <div>
-                    <h3 className="float-md-start mb-0">Cover_111</h3>
-                    <nav className="nav nav-masthead justify-content-center float-md-end">
-                        <a className="nav-link active" aria-current="page" href="/">Home</a>
-                        <a className="nav-link" href="/">Contact</a>
+                    <h5 class="float-md-start font-monospace">Prof Kitney's Lab</h5>
+                    <nav class="nav nav-masthead justify-content-center float-md-end">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <a class="nav-link" href="https://www.imperial.ac.uk/synthetic-biology">Contact</a>
                     </nav>
                 </div>
             </header>
 
-            <main className="px-4">
-                <h1>Cover your page.</h1>
-                <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download,
-                    edit the text, and add your own fullscreen background photo to make it your own.</p>
-                <p className="lead">
-                    <a href="/" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+            <main class="px-4">
+                <h1 float-sm-start>Laboratory Inventory Management System (LIMS)</h1>
+                <p class="lead">LIMS is designed to track and organize the samples in the lab, considered as one of
+                    the most efficient platforms for experimental data recording and management.</p>
+                <p class="lead">
+                    <div className="container">
+                        <div className="row g-1">
+                            <div className="col-3">
+                                <NavLink to='/log'>
+                                    <button class="btn btn-outline-primary btn-lg" type="button">Log</button>
+                                </NavLink>
+                            </div>
+                            <div className="col-3">
+                                <NavLink to='/search'>
+                                    <button className="btn btn-outline-secondary btn-lg" type="button">Search</button>
+                                </NavLink>
+                            </div>
+                            <div className="col-3">
+                                 <NavLink to='/check'>
+                                     <button className="btn btn-outline-success btn-lg" type="button">Check</button>
+                                 </NavLink>
+                            </div>
+                            <div className="col-3">
+                                <NavLink to='/view'>
+                                    <button className="btn btn-outline-danger btn-lg" type="button">View</button>
+                                </NavLink>
+                            </div>
+                        </div>
+                    </div>
+
                 </p>
+
+
             </main>
 
-            <footer className="mt-auto text-white-50">
-                <p>Cover template for <a href="https://getbootstrap.com/" className="text-white">Bootstrap</a>, by <a
-                    href="https://twitter.com/mdo" className="text-white">@mdo</a>.</p>
+            <div className="content">
+                <Route path="/log" component={Log}/>
+            </div>
+
+            <footer class="mt-auto text-white-50">
+                <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a
+                    href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
             </footer>
         </div>
 
         </body>
+        </HashRouter>
     );
 
 }
---
+++ frontend/src/pages/Log.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Log extends Component {
+  render() {
+    return (
+        <div>
+            <div>HomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHome</div>
+        </div>
+    );
+  }
+}
+
+export default Log;
